dfe27db30cda1d54f439653aab5c0335556e1304
Derek O'Kelly <derekokelly1997@gmail.com>
2018-10-31 00:29:38 +0000

    Implemented Module class

--- src/main/java/ie/nuigalway/derekokelly/ct417assignment1/Module.java
+++ src/main/java/ie/nuigalway/derekokelly/ct417assignment1/Module.java
@@ -1,9 +1,35 @@
 package ie.nuigalway.derekokelly.ct417assignment1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author derek
  */
 public class Module {
     
+    private String name, id;
+    private ArrayList<Student> studentList;
+    
+    public Module(String name, String id, ArrayList<Student> list) {
+        this.name = name;
+        this.id = id;
+        this.studentList = list;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getID() {
+        return this.id;
+    }
+    
+    public ArrayList<Student> getStudentList() {
+        return this.studentList;
+    }
+    
+    public void addStudent(Student s) {
+        this.studentList.add(s);
+    }
 }
