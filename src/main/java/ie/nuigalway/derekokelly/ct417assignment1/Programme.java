ec006b21ebda4430330b7686df08b95060c9cb09
Derek O'Kelly <derekokelly1997@gmail.com>
2018-10-31 00:53:27 +0000

    Implemented Programme class

--- src/main/java/ie/nuigalway/derekokelly/ct417assignment1/Programme.java
+++ src/main/java/ie/nuigalway/derekokelly/ct417assignment1/Programme.java
@@ -1,9 +1,43 @@
 package ie.nuigalway.derekokelly.ct417assignment1;
 
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 /**
  *
  * @author derek
  */
 public class Programme {
+    
+    private String name;
+    private ArrayList<Module> modules;
+    private DateTime startDate, endDate;
+    
+    private DateTimeFormatter format = DateTimeFormat.forPattern("dd/mm/yyyy");
+    
+    public Programme(String name, ArrayList<Module> modules, DateTime startDate, DateTime endDate) {
+        this.name = name;
+        this.modules = modules;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public ArrayList<Module> getModules() {
+        return this.modules;
+    }
+    
+    public DateTime getStartDate() {
+        return this.startDate;
+    }
+    
+    public DateTime getEndDate() {
+        return this.endDate;
+    }
     
 }
